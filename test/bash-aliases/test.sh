947d126cdc439113057a2b35d16fd06109e5879a
Daniel Loureiro <loureirorg@gmail.com>
2024-12-07 11:08:09 +0000

    :bug: fix: replace 'bash -ic' with 'bash -lc' in tests for better compatibility in CI
    Updated the test script to use 'bash -lc' instead of 'bash -ic' to ensure the aliases are loaded correctly in the CI environment.

--- test/bash-aliases/test.sh
+++ test/bash-aliases/test.sh
@@ -15,15 +15,15 @@ mkdir -p "$PWD/.devcontainer/etc/bash-aliases"
 echo "alias testalias='echo Hello, World!'" > "$PWD/.devcontainer/etc/bash-aliases/test-aliases.sh"
 
 # Feature-specific tests
-check "alias is loaded" bash -ic "alias testalias | grep 'echo Hello, World!'"
-check "alias works" bash -ic "testalias | grep 'Hello, World!'"
+check "alias is loaded" bash -lc "alias testalias | grep 'echo Hello, World!'"
+check "alias works" bash -lc "testalias | grep 'Hello, World!'"
 
 # Test when there's no bash-aliases directory.
 rm -rf "$PWD/.devcontainer/etc/bash-aliases"
-check "no bash-aliases directory" bash -ic "echo 'No errors'"
+check "no bash-aliases directory" bash -lc "echo 'No errors'"
 
 # "testalias" alias should not be available anymore
-check "alias is not loaded" bash -ic "alias testalias 2>&1 | grep 'not found'"
+check "alias is not loaded" bash -lc "alias testalias 2>&1 | grep 'not found'"
 
 # Report result
 reportResults
